785966543a1de39713d1f5834d7d88333e3d86d6
Lukxsx <33071953+Lukxsx@users.noreply.github.com>
2020-11-26 20:03:22 +0200

    Added cucumber tests

--- src/test/java/lukuvinkkikirjasto/ui/Stepdefs.java
+++ src/test/java/lukuvinkkikirjasto/ui/Stepdefs.java
@@ -15,9 +15,8 @@ import static org.junit.Assert.*;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 
-
 public class Stepdefs {
-        
+
     SystemIO io;
     Database fakeDatabase = new FakeDatabase();
     ReadingTipService rtService;
@@ -28,7 +27,7 @@ public class Stepdefs {
         rtService = new ReadingTipService(fakeDatabase);
         ui = new UserInterface(io, rtService);
     }
-    
+
     @Given("command {string} is selected")
     public void commandIsSelected(String string) {
         ui.chooseCommand(string).execute();
@@ -38,7 +37,7 @@ public class Stepdefs {
     public void messageIsShown(String message) {
         verify(io).output(message + "\n");
     }
-    
+
     @When("Header {string} and description {string} are given.")
     public void headerAndDescriptionAreGiven(String header, String description) throws SQLException {
         rtService.add(new ReadingTip(header, description));
@@ -48,5 +47,15 @@ public class Stepdefs {
     public void theBookIsAdded(String header) throws SQLException {
         List<ReadingTip> tipList = rtService.getTips();
         assertTrue(tipList.get(0).getTitle().equals(header));
+    }
+
+    @Given("tip with header {string} and description {string} is added")
+    public void tipWithHeaderAndDescriptionIsAdded(String string, String string2) throws SQLException {
+        rtService.add(new ReadingTip(string, string2));
+    }
+
+    @Then("tip with header {string} and description {string} is listed")
+    public void tipWithHeaderAndDescriptionIsListed(String string, String string2) {
+        verify(io).output("Header: " + string + "\n" + "Description: " + string2 + "\n");
     }
 }
--- src/test/resources/lukuvinkkikirjasto/listing.feature
+++ src/test/resources/lukuvinkkikirjasto/listing.feature
@@ -3,3 +3,8 @@ Feature: The reading tips can be listed.
     Scenario: an empty list gives a "No tips" -notification
         Given command "list" is selected
         Then "No tips" message is shown
+
+    Scenario: list command lists tip if a tip is added
+        Given tip with header "Hello Ruby" and description "intro to coding" is added
+        And command "list" is selected
+        Then tip with header "Hello Ruby" and description "intro to coding" is listed
