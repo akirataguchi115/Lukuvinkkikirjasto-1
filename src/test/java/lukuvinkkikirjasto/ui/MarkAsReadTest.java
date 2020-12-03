3eb3a93ca9344852032c8aa89a05dccc68e9c922
Juha-Pekka Tulijoki <jp.tulijoki@gmail.com>
2020-12-03 21:53:41 +0200

    remove default comments from mark as read test

--- src/test/java/lukuvinkkikirjasto/ui/MarkAsReadTest.java
+++ src/test/java/lukuvinkkikirjasto/ui/MarkAsReadTest.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package lukuvinkkikirjasto.ui;
 
 import java.sql.SQLException;
@@ -14,10 +9,6 @@ import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-/**
- *
- * @author tulijoki
- */
 public class MarkAsReadTest {
     
     IO io;
